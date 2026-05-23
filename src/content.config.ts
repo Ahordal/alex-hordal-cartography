5940f07dd20e2f188cd2385148d3b6742d615247
Spnkmyr <alex@ullrmaps.com>
2026-05-22 23:22:38 -0300

    Added projects to the portfolio, and worked on layout

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,30 +8,29 @@ const projects = defineCollection({
     base: "./src/content/projects",
   }),
 
-  
-  schema: () => z.object({
+
+  schema: ({ image }) => z.object({
     title: z.string(),
-    description: z.string(),        
+    description: z.string(),
     plainTitle: z.string(),
     category: z.enum([
-      "GIS Platforms",
-      "Web Mapping & Development",
-      "Programming & GIS Automation",
-      "Cartography & Geovisualization",
-      "Design & Visualization Tools",
-      "Databases & Data Management",
+      "Interactive GIS",
+      "Data Visualization",
+      "Cartographic Design",
+      "Spatial Analysis",
+      "Terrain & Relief Visualization",
     ]),
+    
     technologies: z.array(z.string()),
-    tags: z.array(z.string()),
-    featured: z.boolean().default(false),
+   
     appliedSkills: z.array(z.string()).optional(),
-    layoutClass: z.string().optional(),
     downloadFile: z.string().optional(),
     downloadLabel: z.string().optional(),
     listClass: z.string().optional(),
-    heroCaption: z.string().optional()
-
-
+    objectPosition: z.string().optional(),
+    heroCaption: z.string().optional(),
+    video: z.string().optional(),
+    featuredImage: image()
   }),
 });
 
