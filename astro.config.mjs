eeb570b08f373d0d377c6abcac51aa30df05704b
Spnkmyr <alex@ullrmaps.com>
2026-05-25 15:16:53 -0300

    Add vercel adapter and update form scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel"; 
 
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  output: "hybrid",  
+  adapter: vercel(),  
 });
